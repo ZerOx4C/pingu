d5ab0d3007787d036d8516ca9a7a7fc5fbee20ec
ZerOx4C <zerox4c@gmail.com>
2024-05-16 13:13:15 +0900

    ログ周りを改善した

--- run.bat
+++ run.bat
@@ -1,15 +1,23 @@
 @echo off
 
-type nul > status.log
+set LOGNAME=%date%_%time%
+set LOGNAME=%LOGNAME:/=%
+set LOGNAME=%LOGNAME::=%
+set LOGNAME=%LOGNAME:.=%
+set LOGNAME=%LOGNAME%.log
+
+set IPADDR=%1
+
+echo %IPADDR% > %LOGNAME%
 
 :head
 
-ping -n 1 %1 | findstr TTL > nul
+ping -n 1 %IPADDR% | findstr TTL > nul
 
 set DATETIME=%date% %time%
 
 if errorlevel 1 (
-	set STAT=NG
+	set STAT=NG!
 ) else (
 	set STAT=OK
 )
@@ -20,7 +28,7 @@ if %STAT% == OK (
 	echo [91m%DATETIME% %STAT%[0m
 )
 
-echo %DATETIME% %STAT% >> status.log
+echo %DATETIME% %STAT% >> %LOGNAME%
 
 timeout /t 60 /nobreak > nul
 
